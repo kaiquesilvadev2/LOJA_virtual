6e72cc1bd35b2c676022ca73a795dba67bbbc8e7
kaiquesilvadev2 <j.kaiquesilva1570@gmail.com>
2024-08-15 19:50:05 -0300

    properties do h2 para facilitar teste e primeira classe para testa o spring e o projeto

---
+++ src/main/java/com/kaique/lojaVirtual/doman/entity/MarcaProduto.java
@@ -0,0 +1,60 @@
+package com.kaique.lojaVirtual.doman.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_marca_produto")
+public class MarcaProduto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nomeDesc;
+
+	public MarcaProduto() {
+	}
+
+	public MarcaProduto(Long id, String nomeDesc) {
+		this.id = id;
+		this.nomeDesc = nomeDesc;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return nomeDesc;
+	}
+
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarcaProduto other = (MarcaProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+}
