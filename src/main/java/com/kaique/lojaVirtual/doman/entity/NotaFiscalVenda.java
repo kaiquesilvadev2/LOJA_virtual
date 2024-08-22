2d6db3a202e2a23e7d2e2463311446a3544f5d1e
kaiquesilvadev2 <j.kaiquesilva1570@gmail.com>
2024-08-22 19:44:59 -0300

    classe @Column(columnDefinition = TEXT)

---
+++ src/main/java/com/kaique/lojaVirtual/doman/entity/NotaFiscalVenda.java
@@ -0,0 +1,120 @@
+package com.kaique.lojaVirtual.doman.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_Nota_fiscal_venda")
+public class NotaFiscalVenda {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String numero;
+	private String serie;
+	private String tipo;
+
+	@Column(columnDefinition = "TEXT")
+	private String xml;
+
+	@Column(columnDefinition = "TEXT")
+	private String pdf;
+
+	@OneToOne
+	private VendaCompraVirtual vendaCompraVirtual;
+
+	public NotaFiscalVenda() {
+	}
+
+	public NotaFiscalVenda(Long id, String numero, String serie, String tipo, String xml, String pdf,
+			VendaCompraVirtual vendaCompraVirtual) {
+		this.id = id;
+		this.numero = numero;
+		this.serie = serie;
+		this.tipo = tipo;
+		this.xml = xml;
+		this.pdf = pdf;
+		this.vendaCompraVirtual = vendaCompraVirtual;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getSerie() {
+		return serie;
+	}
+
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getXml() {
+		return xml;
+	}
+
+	public void setXml(String xml) {
+		this.xml = xml;
+	}
+
+	public String getPdf() {
+		return pdf;
+	}
+
+	public void setPdf(String pdf) {
+		this.pdf = pdf;
+	}
+
+	public VendaCompraVirtual getVendaCompraVirtual() {
+		return vendaCompraVirtual;
+	}
+
+	public void setVendaCompraVirtual(VendaCompraVirtual vendaCompraVirtual) {
+		this.vendaCompraVirtual = vendaCompraVirtual;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotaFiscalVenda other = (NotaFiscalVenda) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
