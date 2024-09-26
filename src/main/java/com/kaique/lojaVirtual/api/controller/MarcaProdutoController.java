1d7fb993269c29f553bab3fb25075e4a30fdbeaf
kaiquesilvadev2 <j.kaiquesilva1570@gmail.com>
2024-09-26 19:14:41 -0300

    CRUD de marca produto ok

--- src/main/java/com/kaique/lojaVirtual/api/controller/MarcaProdutoController.java
+++ src/main/java/com/kaique/lojaVirtual/api/controller/MarcaProdutoController.java
@@ -2,7 +2,11 @@ package com.kaique.lojaVirtual.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,9 +25,25 @@ public class MarcaProdutoController {
 	@Autowired
 	private MarcaProdutoService servives;
 
+	@GetMapping("/{id}")
+	public MarcaResponseDto buscaPorId(@PathVariable Long id) {
+		return new MarcaResponseDto(servives.buscaPorId(id));
+	}
+
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public MarcaResponseDto salvaPessoaUser(@Valid @RequestBody MarcaRequestDto dto) {
 		return new MarcaResponseDto(servives.Salva(dto));
+	}
+
+	@PutMapping("/{id}")
+	public MarcaResponseDto atualizar(@Valid @RequestBody MarcaRequestDto dto, @PathVariable Long id) {
+		return new MarcaResponseDto(servives.atualizar(dto, id));
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		servives.deletar(id);
 	}
 }
--- src/main/java/com/kaique/lojaVirtual/domain/services/MarcaProdutoService.java
+++ src/main/java/com/kaique/lojaVirtual/domain/services/MarcaProdutoService.java
@@ -1,12 +1,16 @@
 package com.kaique.lojaVirtual.domain.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kaique.lojaVirtual.domain.dto.request.MarcaRequestDto;
 import com.kaique.lojaVirtual.domain.entity.MarcaProduto;
+import com.kaique.lojaVirtual.domain.exceptions.EntidadeEmUsoException;
 import com.kaique.lojaVirtual.domain.exceptions.EntidadeExistenteException;
+import com.kaique.lojaVirtual.domain.exceptions.EntidadeNaoEncontradaException;
 import com.kaique.lojaVirtual.domain.repositories.MarcaProdutoRepository;
 
 @Service
@@ -14,23 +18,51 @@ public class MarcaProdutoService {
 
 	@Autowired
 	private MarcaProdutoRepository repository;
-	
-	
+
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public MarcaProduto buscaPorId(Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("ID de código '" + id + "' não encontrado ."));
+	}
+
 	@Transactional
 	public MarcaProduto Salva(MarcaRequestDto Dto) {
-		
-		if(repository.buscaDescricao(Dto.getNomeDesc()).isPresent()) {
+
+		if (repository.buscaDescricao(Dto.getNomeDesc()).isPresent()) {
 			throw new EntidadeExistenteException("Essa descrição de marca já está presente no banco de dados.");
 		}
-		
-		return repository.save(convertdto(Dto));
+
+		return repository.save(convertdto(Dto, new MarcaProduto()));
 	}
-	
-	private MarcaProduto convertdto(MarcaRequestDto Dto) {
-		
-		MarcaProduto marcaProduto = new MarcaProduto();
+
+	@Transactional
+	public MarcaProduto atualizar(MarcaRequestDto Dto, Long id) {
+
+		MarcaProduto marcaProduto = buscaPorId(id);
+
+		if (repository.buscaDescricao(Dto.getNomeDesc()).isPresent()) {
+			throw new EntidadeExistenteException("Essa descrição de marca já está presente no banco de dados.");
+		}
+
+		return repository.save(convertdto(Dto, marcaProduto));
+	}
+
+	@Transactional
+	public void deletar(Long id) {
+
+		try {
+			buscaPorId(id);
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(id);
+		}
+
+	}
+
+	private MarcaProduto convertdto(MarcaRequestDto Dto, MarcaProduto marcaProduto) {
+
 		marcaProduto.setNomeDesc(Dto.getNomeDesc());
-		
+
 		return marcaProduto;
 	}
 }
