49bb395832c33408c90b32abb34c630f87eff05e
kaiquesilvadev2 <j.kaiquesilva1570@gmail.com>
2025-04-18 18:43:23 -0300

    organização de projetos

---
+++ LOJA_virtual/src/main/java/com/kaique/lojaVirtual/domain/enuns/StatusContaReceber.java
@@ -0,0 +1,27 @@
+package com.kaique.lojaVirtual.domain.enuns;
+
+public enum StatusContaReceber {
+	
+	COBRANCA("Receber"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada"),
+	RENEGOCIADA("Renegociada");
+	
+	
+	private String descricao;
+	
+	private StatusContaReceber(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+
+}
