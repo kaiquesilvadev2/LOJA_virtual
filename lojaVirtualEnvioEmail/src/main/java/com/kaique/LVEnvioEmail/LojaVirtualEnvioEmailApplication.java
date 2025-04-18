722034a36520af8fabfb28f548e347e7b7e83fe4
kaiquesilvadev2 <j.kaiquesilva1570@gmail.com>
2025-04-18 19:13:24 -0300

    projeto para enviar email com conexão por mensageria

---
+++ lojaVirtualEnvioEmail/src/main/java/com/kaique/LVEnvioEmail/LojaVirtualEnvioEmailApplication.java
@@ -0,0 +1,13 @@
+package com.kaique.LVEnvioEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualEnvioEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualEnvioEmailApplication.class, args);
+	}
+
+}
